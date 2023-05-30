8a65ed3f46a9cb3b307392adb2753646766084ea
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-05-31 00:19:01 +0900

    Test. RDS 연결 확인

--- src/main/java/com/saessak/momo/TestController.java
+++ src/main/java/com/saessak/momo/TestController.java
@@ -1,12 +1,18 @@
 package com.saessak.momo;
 
+import com.saessak.momo.mapper.TestMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Slf4j
+@RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final TestMapper mapper;
+
     @GetMapping("/test")
     public String hello() {
         return "hello";
@@ -15,5 +21,10 @@ public class TestController {
     @GetMapping("/test2")
     public String hello2() {
         return "SUCCESS";
+    }
+
+    @GetMapping("/test3")
+    public String hello3() throws Exception {
+        return mapper.test();
     }
 }
