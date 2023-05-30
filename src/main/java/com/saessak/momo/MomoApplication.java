4b5e4b33d0e2fd3ccf57439da29945c2b9df40e8
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-05-31 00:42:11 +0900

    Test. RDS 연결 확인

--- src/main/java/com/saessak/momo/MomoApplication.java
+++ src/main/java/com/saessak/momo/MomoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.saessak.momo.mapper")
+@MapperScan("com.saessak")
 public class MomoApplication {
 
 	public static void main(String[] args) {
