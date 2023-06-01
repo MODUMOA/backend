9e29f5c6710d87f59967548fb74c2fe30a473bae
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-06-01 15:01:14 +0900

    Feat. 로그인 기능 구현
    - 검증, 암호화 로직 X

--- src/main/java/com/saessak/momo/user/model/mapper/UserMapper.java
+++ src/main/java/com/saessak/momo/user/model/mapper/UserMapper.java
@@ -1,11 +1,15 @@
 package com.saessak.momo.user.model.mapper;
 
 import com.saessak.momo.user.dto.SignupForm;
+import com.saessak.momo.user.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 @Mapper
 public interface UserMapper {
     void insertUser(SignupForm form) throws SQLException;
+
+    UserDto selectUser(Map<String, String> form) throws SQLException;
 }
--- src/main/java/com/saessak/momo/user/model/service/UserServiceImpl.java
+++ src/main/java/com/saessak/momo/user/model/service/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.saessak.momo.user.model.service;
 
 import com.saessak.momo.user.dto.SignupForm;
+import com.saessak.momo.user.dto.UserDto;
 import com.saessak.momo.user.model.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -17,5 +20,17 @@ public class UserServiceImpl implements UserService {
         log.info("form={}", form);
         userMapper.insertUser(form);
 
+    }
+
+    @Override
+    public UserDto login(Map<String, String> form) throws Exception {
+        log.info("form={}", form);
+        UserDto findUser = userMapper.selectUser(form);
+
+        if (findUser == null || !findUser.getUserPwd().equals(form.get("userPwd"))) {
+            return null;
+        }
+
+        return findUser;
     }
 }
