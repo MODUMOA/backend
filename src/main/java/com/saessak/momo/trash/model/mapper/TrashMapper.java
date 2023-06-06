fa2c1f9abd1ab84ad21d2e828df68527ea4d9574
hajin618 <88872527+hajin618@users.noreply.github.com>
2023-06-06 21:54:45 +0900

    feat: add DashBoard api

--- src/main/java/com/saessak/momo/trash/model/mapper/TrashMapper.java
+++ src/main/java/com/saessak/momo/trash/model/mapper/TrashMapper.java
@@ -1,7 +1,17 @@
 package com.saessak.momo.trash.model.mapper;
 
+import com.saessak.momo.trash.dto.PrevPercentParam;
+import com.saessak.momo.trash.dto.WeeklyEmissionParam;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TrashMapper {
+    // 오늘 요일 구하기
+    int getNowDay() throws Exception;
+
+    // 일주일 배출량 구하기
+    int getWeeklyEmission(WeeklyEmissionParam param) throws Exception;
+
+    // 전 주(전전주) 배출량 구하기
+    int getPrevEmission(PrevPercentParam param) throws Exception;
 }
