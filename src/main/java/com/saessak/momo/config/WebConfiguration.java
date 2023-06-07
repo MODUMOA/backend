a20afbd63f812d55d2da3f01021d9561d6c2157d
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-06-07 18:26:01 +0900

    fix.Config 수정

--- src/main/java/com/saessak/momo/config/WebConfiguration.java
+++ src/main/java/com/saessak/momo/config/WebConfiguration.java
@@ -11,7 +11,8 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081") // 나중에 AWS 주소 추가해야함
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://43.202.34.154"
+                                ,"https://43.202.34.154", "http://modumoa.shop", "https://modumoa.shop") // 나중에 AWS 주소 추가해야함
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
