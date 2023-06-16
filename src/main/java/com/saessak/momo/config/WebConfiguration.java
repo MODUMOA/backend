7931fd39585c74ddd54402e0ae4a89c0cf752aa3
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-06-16 09:35:10 +0900

    fix. 최종점검

--- src/main/java/com/saessak/momo/config/WebConfiguration.java
+++ src/main/java/com/saessak/momo/config/WebConfiguration.java
@@ -13,7 +13,7 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://43.202.34.154:80", "http://10.0.2.2:8080",
                                 "http://192.168.159.116:8080", "http://192.168.159.209:8080", "http://192.168.76.178:8080", "http://192.168.76.209:8080", "http://192.168.154.178:8080", "http://192.168.0.102:8080"
-                                ,"http://192.168.0.178:8080","http://192.168.1.28:8080",  "http://192.168.0.116:8080", "http://192.168.0.100:8080", "http://192.168.154.209:8080",
+                                ,"http://192.168.0.178:8080","http://192.168.103.64:8080",  "http://192.168.0.116:8080", "http://192.168.0.100:8080", "http://192.168.154.209:8080",
                                 "https://43.202.34.154:80", "http://modumoa.shop", "https://modumoa.shop") // 나중에 AWS 주소 추가해야함
                 .allowedMethods("*")
                 .allowedHeaders("*")
