4335f6ed866d8a656fe757f76caf1b71bc145657
Kang Dong Pyo <76652908+97Kzone@users.noreply.github.com>
2023-06-16 09:40:04 +0900

    fix. 최종점검

--- src/main/java/com/saessak/momo/config/WebConfiguration.java
+++ src/main/java/com/saessak/momo/config/WebConfiguration.java
@@ -11,10 +11,11 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://43.202.34.154:80", "http://10.0.2.2:8080",
-                                "http://192.168.159.116:8080", "http://192.168.159.209:8080", "http://192.168.76.178:8080", "http://192.168.76.209:8080", "http://192.168.154.178:8080", "http://192.168.0.102:8080"
-                                ,"http://192.168.0.178:8080","http://192.168.103.64:8080",  "http://192.168.0.116:8080", "http://192.168.0.100:8080", "http://192.168.154.209:8080",
-                                "https://43.202.34.154:80", "http://modumoa.shop", "https://modumoa.shop") // 나중에 AWS 주소 추가해야함
+//                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://43.202.34.154:80", "http://10.0.2.2:8080",
+//                                "http://192.168.159.116:8080", "http://192.168.159.209:8080", "http://192.168.76.178:8080", "http://192.168.76.209:8080", "http://192.168.154.178:8080", "http://192.168.0.102:8080"
+//                                ,"http://192.168.0.178:8080","http://192.168.103.64:8080",  "http://192.168.0.116:8080", "http://192.168.0.100:8080", "http://192.168.154.209:8080",
+//                                "https://43.202.34.154:80", "http://modumoa.shop", "https://modumoa.shop") // 나중에 AWS 주소 추가해야
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
